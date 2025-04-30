595ea017cbfd8bff4db95c7953f35d867d5437ff
saylessis <beshaneh@gmail.com>
2025-04-30 23:54:06 +0300

    color change

--- src/components/constant/color.tsx
+++ src/components/constant/color.tsx
@@ -1,5 +1,5 @@
 export const maincolor = '#3A59D1'
-export const seconderycolor = "#3D90D7"
+export const seconderycolor = "#16C47F"
 export const Accent = "#7AC6D2"
 export const backgroundcolor = "#F1EFEC"
 export const fontColor = '#333333'
--- src/components/course.jsx
+++ src/components/course.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { backgroundcolor, maincolor, seconderycolor } from "../components/constant/color";
+import { Accent, backgroundcolor, maincolor, seconderycolor } from "../components/constant/color";
 import { motion } from "framer-motion";
 
 const Coursebox = (props) => {
     return (
-        <motion.div whileHover={{ backgroundColor: `${seconderycolor}55`, cursor: 'pointer' }} style={{ backgroundColor: backgroundcolor, width: 250, borderRadius: 5 }}>
+        <motion.div whileHover={{ backgroundColor: `${Accent}55`, cursor: 'pointer' }} style={{ backgroundColor: seconderycolor, width: 250, borderRadius: 5 }}>
             <motion.h3 style={{ padding: 20, textAlign: 'center' }}>{props.title}</motion.h3>
         </motion.div>
     );
